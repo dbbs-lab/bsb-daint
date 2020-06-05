dc5e1bdb66c41852fef612badd6876809ef17035
Robin De Schepper <robingilbert.deschepper@unipv.it>
2020-06-05 12:30:04 +0200

    Update modules.sh

--- helpers/modules.sh
+++ helpers/modules.sh
@@ -1,3 +1,8 @@
+# Python 3 & mpi4py
 module load cray-python/3.6.5.7
+# Cython
+module load PyExtensions/3.6.5.7-CrayGNU-19.10
+# pip
 module load pip/20.0.2-py3
+# h5py
 module load cray-hdf5/1.10.5.1
