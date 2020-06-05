5c906ecc31776671559978134622a244a8d621ea
Robin De Schepper <robingilbert.deschepper@unipv.it>
2020-06-05 12:37:52 +0200

    Update modules.sh

--- helpers/modules.sh
+++ helpers/modules.sh
@@ -1,3 +1,5 @@
+# Multicore stack
+module load daint-mc
 # Python 3 & mpi4py
 module load cray-python/3.6.5.7
 # Cython
@@ -5,4 +7,4 @@ module load PyExtensions/3.6.5.7-CrayGNU-19.10
 # pip
 module load pip/20.0.2-py3
 # h5py
-module load cray-hdf5/1.10.5.1
+module load h5py/2.8.0-CrayGNU-19.10-python3-parallel
