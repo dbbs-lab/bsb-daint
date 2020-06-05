470f12599c47a18baecc129f3a1f577c28896a40
Robin De Schepper <robingilbert.deschepper@unipv.it>
2020-06-05 12:26:37 +0200

    Delete install-mpi4py.sh
    This is not required, it was added for when pip couldn't find `mpi4py` under `/opt` but this was not a recurring problem

--- helpers/install-mpi4py.sh
+++
@@ -1,4 +0,0 @@
-sitepack=$(python -c "import site; print(site.getsitepackages()[0]);")
-userpack=$(python -c "import site; print(site.getusersitepackages());")
-cp -r $sitepack/mpi4py $userpack
-cp -r $sitepack/mpi4py-* $userpack
