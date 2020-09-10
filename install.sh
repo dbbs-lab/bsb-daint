7fea3e988d6710fa34b6e61d105f5883980e5816
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-10 16:10:24 +0200

    removed mpi4py install script (it can be loaded from the modules)

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/helpers/modules.sh
-source $DIR/helpers/install-mpi4py.sh
 
 # Install dbbs scaffold package
 pip install --user bsb==3.4.0b0
