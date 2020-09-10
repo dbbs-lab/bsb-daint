c96f9cbb52883614747e187700e8d32bbce38654
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-10 16:08:23 +0200

    updated module list in reponse to the system update of 09/09/2020

--- helpers/modules.sh
+++ helpers/modules.sh
@@ -1,12 +1,12 @@
 # Multicore stack
 module load daint-mc
 # Python 3 & mpi4py
-module load cray-python/3.6.5.7
+module load cray-python/3.8.2.1
 # Cython
-module load PyExtensions/3.6.5.7-CrayGNU-19.10
+module load PyExtensions/python3-CrayGNU-20.08
 # pip
 module load pip/20.0.2-py3
 # h5py
-module load h5py/2.8.0-CrayGNU-19.10-python3-parallel
+module load h5py/2.10.0-CrayGNU-20.08-python3-parallel
 # GSL for NEST
-module load GSL
+module load GSL/2.5-CrayGNU-20.08
