a4314400264b2caa2d7f9ad43e3f2ab89842e38e
Alice Geminiani <alice.geminiani@polimi.it>
2020-09-15 18:18:13 +0200

    Removed export

--- install-nest-modules.sh
+++ install-nest-modules.sh
@@ -8,8 +8,6 @@ cd build
 CEREBNEST_BUILD_DIR=$PWD
 export NEST_INSTALL_DIR=$HOME/nest-2.18.0-install
 cmake -Dwith-nest=${NEST_INSTALL_DIR}/bin/nest-config ..
-export NEST_MODULE_PATH=${NEST_INSTALL_DIR}/lib/nest:$NEST_MODULE_PATH
-export SLI_PATH=${NEST_INSTALL_DIR}/share/nest/sli:$SLI_PATH
-make
+make -j
 make install
 cd $PREVIOUS_DIR
