5d1441d7658a04ae292ec93cf0d6a45c39f7949d
Alice Geminiani <alice.geminiani@polimi.it>
2020-09-15 17:39:56 +0200

    Update nest-install to use dbbs fork
    2.18.0 compatible with Python 3.8 https://github.com/nest/nest-simulator/pull/1340

--- install-nest.sh
+++ install-nest.sh
@@ -5,12 +5,19 @@ module load PyExtensions
 export CRAYPE_LINK_TYPE=dynamic 
 
 cd
-wget -P $HOME "https://zenodo.org/record/2605422/files/nest-simulator-2.18.0.tar.gz"
-tar -xzf nest-simulator-2.18.0.tar.gz
+git clone https://github.com/dbbs-lab/nest-simulator.git --branch 2.18.0 --depth 1
 rm -rf nest-2.18.0-install/
 mkdir nest-2.18.0-install/
 cd nest-2.18.0-install
 
-cmake -Dwith-mpi=ON -Dwith-python=3 -DPYTHON_EXECUTABLE=/opt/python/3.8.2.1/bin/python -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -Dstatic-libraries=OFF -DCMAKE_INSTALL_PREFIX:PATH=$HOME/nest-2.18.0-install $HOME/nest-simulator-2.18.0/
+cmake \
+  -Dwith-mpi=ON \
+  -Dwith-python=3 \
+  -DPYTHON_EXECUTABLE=/opt/python/3.8.2.1/bin/python \
+  -DCMAKE_CXX_COMPILER=CC \
+  -DCMAKE_C_COMPILER=cc \
+  -Dstatic-libraries=OFF \
+  -DCMAKE_INSTALL_PREFIX:PATH=$HOME/nest-2.18.0-install \
+  $HOME/nest-simulator/
 make
 make install
