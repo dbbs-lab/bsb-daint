55314ace5bc7e5f0051b02302d10e99c652923eb
Alice Geminiani <alice.geminiani@polimi.it>
2020-09-15 16:57:03 +0200

    Update nest_export.sh to 3.8

--- helpers/nest_export.sh
+++ helpers/nest_export.sh
@@ -1,6 +1,6 @@
 # Environment variables for NEST
 export PATH=$PATH:$HOME/nest-2.18.0-install/bin
-export PYTHONPATH=$HOME/nest-2.18.0-install/lib64/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=$HOME/nest-2.18.0-install/lib64/python3.8/site-packages:$PYTHONPATH
 export NEST_MODULE_PATH=$NEST_MODULE_PATH:$HOME/nest-2.18.0-install/lib64/nest
 export SLI_PATH=$SLI_PATH:$HOME/nest-2.18.0-install/share/nest/sli
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/nest-2.18.0-install/lib64/nest
