9d8cb6e4f3ad39b02cde97a5e53a32b32cac129d
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-15 17:48:48 +0200

    make j

--- install-nest.sh
+++ install-nest.sh
@@ -2,7 +2,7 @@ module load daint-mc
 module swap PrgEnv-cray PrgEnv-gnu
 module load PyExtensions
 
-export CRAYPE_LINK_TYPE=dynamic 
+export CRAYPE_LINK_TYPE=dynamic
 
 cd
 git clone https://github.com/dbbs-lab/nest-simulator.git --branch 2.18.0 --depth 1
@@ -19,5 +19,5 @@ cmake \
   -Dstatic-libraries=OFF \
   -DCMAKE_INSTALL_PREFIX:PATH=$HOME/nest-2.18.0-install \
   $HOME/nest-simulator/
-make
+make -j
 make install
