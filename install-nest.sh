7e331c320d0b1c6c309a3199d1ce297f6a7ad190
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-28 19:46:36 +0200

    fixed GSL error

--- install-nest.sh
+++ install-nest.sh
@@ -1,11 +1,13 @@
 module load daint-mc
-module unload PrgEnv-gnu
-module load PrgEnv-cray
-module load PyExtensions
+module unload PrgEnv-gnu/6.0.8
+module load PrgEnv-cray/6.0.8
+module load PyExtensions/python3-CrayGNU-20.08
+module load GSL/2.5-CrayGNU-20.08
 
 export CRAYPE_LINK_TYPE=dynamic
 
 cd
+rm -rf nest-simulator
 git clone https://github.com/dbbs-lab/nest-simulator.git --branch 2.18.0 --depth 1
 rm -rf nest-2.18.0-install/
 mkdir nest-2.18.0-install/
