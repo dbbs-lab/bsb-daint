e7ee0279449e246b4e7745c536572b9a1d50f977
Robin De Schepper <robin.deschepper93gmail.com>
2021-03-16 17:33:49 +0100

    fixed updated toolchain

--- helpers/modules.sh
+++ helpers/modules.sh
@@ -3,8 +3,8 @@ module load daint-mc
 # Python 3 & mpi4py
 module load cray-python/3.8.2.1
 # Cython
-module load PyExtensions/python3-CrayGNU-20.08
+module load PyExtensions/python3-CrayGNU-20.11
 # h5py
-module load h5py/2.10.0-CrayGNU-20.08-python3-parallel
+module load h5py/2.10.0-CrayGNU-20.11-python3-parallel
 # GSL for NEST
-module load GSL/2.5-CrayGNU-20.08
+module load GSL/2.5-CrayGNU-20.11
