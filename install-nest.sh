da609ec86e89175a8cf49239e73778833b5a365e
Alice Geminiani <alice.geminiani@polimi.it>
2021-03-24 14:59:15 +0100

    Update nest-install.sh for Daint upgrade March 2021

--- install-nest.sh
+++ install-nest.sh
@@ -1,8 +1,8 @@
 module load daint-mc
-module unload PrgEnv-gnu/6.0.8
-module load PrgEnv-cray/6.0.8
-module load PyExtensions/python3-CrayGNU-20.08
-module load GSL/2.5-CrayCCE-20.08
+module unload PrgEnv-gnu/6.0.9
+module load PrgEnv-cray/6.0.0
+module load PyExtensions/python3-CrayGNU-20.11
+module load GSL/2.5-CrayCCE-20.11
 
 export CRAYPE_LINK_TYPE=dynamic
 
@@ -17,9 +17,9 @@ rm -rf nest-2.18.0-install/
 cmake \
   -Dwith-mpi=ON \
   -Dwith-python=3 \
-  -DPYTHON_EXECUTABLE=/opt/python/3.8.2.1/bin/python \
-  -DPYTHON_LIBRARY=/opt/python/3.8.2.1/lib/libpython3.8.so \
-  -DPYTHON_INCLUDE_DIR=/opt/python/3.8.2.1/include/python3.8 \
+  -DPYTHON_EXECUTABLE=/opt/python/3.8.5.0/bin/python \
+  -DPYTHON_LIBRARY=/opt/python/3.8.5.0/lib/libpython3.8.so \
+  -DPYTHON_INCLUDE_DIR=/opt/python/3.8.5.0/include/python3.8 \
   -DCMAKE_CXX_COMPILER=CC \
   -DCMAKE_C_COMPILER=cc \
   -Dstatic-libraries=OFF \
