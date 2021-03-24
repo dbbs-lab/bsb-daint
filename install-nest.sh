7f15c561288993938e79157627d84021b46f62b7
Alice Geminiani <alice.geminiani@polimi.it>
2021-03-24 16:24:21 +0100

    Changed install-nest.sh for NEST master

--- install-nest.sh
+++ install-nest.sh
@@ -9,7 +9,7 @@ export CRAYPE_LINK_TYPE=dynamic
 CDIR=$PWD
 cd
 rm -rf nest-simulator
-git clone https://github.com/dbbs-lab/nest-simulator.git --depth 1
+git clone https://github.com/nest/nest-simulator.git --depth 1
 mkdir build
 cd build
 rm -rf nest-2.18.0-install/
