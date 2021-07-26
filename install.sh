b7a0b5e289f2b0fd372ff0595267442284e54a40
Robin De Schepper <robingilbert.deschepper@unipv.it>
2021-07-26 15:50:06 +0200

    Add shared blender 2.91 during install

--- install.sh
+++ install.sh
@@ -8,15 +8,18 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/helpers/modules.sh
-
-ACC=${1:-$ACCOUNT}
-if [ -z $ACC ]; then
-        echo "Either set \$ACCOUNT or pass the account to use as first arg to the command."
-	return 1
-fi
+source $DIR/load.sh
 
 # Install dbbs scaffold package
 python -m pip install --user bsb[NEURON,MPI]==3.7.4 NEURON==7.8.1.1
-
-source $DIR/load.sh
-srun -A $ACC -C mc -p debug glia compile
+# Compile glia prerequisites
+srun -A $ACCOUNT -C mc -p debug glia compile
+# Install Blender
+if [ -d "$STORE/blender" ]; then
+  echo "Blender already installed in $STORE."
+else
+  wget https://download.blender.org/release/Blender2.91/blender-2.91.0-linux64.tar.xz
+  tar -xf blender-2.91.0-linux64.tar.xz
+  mv blender-2.91.0-linux64.tar.xz $STORE/blender
+  rm blender-2.91.0-linux64.tar.xz
+fi
