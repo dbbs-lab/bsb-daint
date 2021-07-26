db46ffdf8c8e8058e7f54c8f0c6dcf82eb5714d7
Robin De Schepper <robingilbert.deschepper@unipv.it>
2021-07-26 14:14:15 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,8 @@
 source $HOME/bsb-daint/load.sh
-alias queue="squeue -u $ACCOUNT"
-alias maa='module avail 2>&1 |grep -i'
-alias debug="srun -C mc -p debug -A $PROJECT $@"
-alias bsb-reconfigure="srun -C mc -p debug -A $PROJECT bsb -v=3 reconfigure $@"
-alias bsb-update="_dir=$PWD && cd $HOME/bsb-daint && git pull && source install.sh $PROJECT && cd $_dir"
+export $STORE="/store/$PROJECT/$ACCOUNT"
 export PATH="$STORE/blender:$PATH"
+alias queue="squeue -u $USER"
+alias maa='module avail 2>&1 |grep -i'
+alias debug="srun -C mc -p debug -A $ACCOUNT $@"
+alias bsb-reconfigure="srun -C mc -p debug -A $ACCOUNT bsb -v=3 reconfigure $@"
+alias bsb-update="_dir=$PWD && cd $HOME/bsb-daint && git pull && source install.sh $ACCOUNT && cd $_dir"
