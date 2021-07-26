8004271a1b6f6d73cefde2fe7089fb777ae3e18e
Robin De Schepper <robingilbert.deschepper@unipv.it>
2021-07-26 13:50:24 +0200

    Forgot to put Blender on PATH

--- shell.sh
+++ shell.sh
@@ -4,3 +4,4 @@ alias maa='module avail 2>&1 |grep -i'
 alias debug="srun -C mc -p debug -A $PROJECT $@"
 alias bsb-reconfigure="srun -C mc -p debug -A $PROJECT bsb -v=3 reconfigure $@"
 alias bsb-update="_dir=$PWD && cd $HOME/bsb-daint && git pull && source install.sh $PROJECT && cd $_dir"
+export PATH="$STORE/blender:$PATH"
