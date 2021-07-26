b57d7da5ec7e7b8fecf1abc10d6f8f1b5f6be8ea
Robin De Schepper <robingilbert.deschepper@unipv.it>
2021-07-26 18:22:41 +0200

    Set DISPLAY

--- shell.sh
+++ shell.sh
@@ -1,6 +1,7 @@
 source $HOME/bsb-daint/load.sh
-export $STORE="/store/$PROJECT/$ACCOUNT"
+export STORE="/store/$PROJECT/$ACCOUNT"
 export PATH="$STORE/blender:$PATH"
+export DISPLAY=:0
 alias queue="squeue -u $USER"
 alias maa='module avail 2>&1 |grep -i'
 alias debug="srun -C mc -p debug -A $ACCOUNT $@"
