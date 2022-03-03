8fd5027e379b4a3ad0e9eb4c06a73ea90e737a55
Robin De Schepper <robingilbert.deschepper@unipv.it>
2022-03-03 12:08:23 +0100

    Fixes #3
    Load a more modern CMake before trying to build nest

--- helpers/modules.sh
+++ helpers/modules.sh
@@ -8,4 +8,5 @@ module load Cython/0.29.25-CrayGNU-21.09
 module load h5py/3.6.0-CrayGNU-21.09-parallel
 # GSL for NEST
 module load GSL/2.7-CrayGNU-21.09
-
+# CMake for installing NEST
+module load CMake
